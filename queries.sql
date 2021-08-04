0c9337fb162c46356d05326a210aa023a8fc13f0
Wajahat Akram <73127111+wajahat414@users.noreply.github.com>
2021-08-04 20:41:03 +0000

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,9 @@
+1 select basics
+-SELECT population FROM world
+  WHERE name = 'Germany'
+-SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+-SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+-
+  

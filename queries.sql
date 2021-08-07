1e6a4868bd7e5dfde4ab4b4f43b62795d5f004d2
Wajahat Akram <73127111+wajahat414@users.noreply.github.com>
2021-08-07 11:58:56 +0000

    completed Lesson1 select

--- queries.sql
+++ queries.sql
@@ -5,5 +5,44 @@
   WHERE name IN ('Sweden', 'Norway', 'Denmark');
 -SELECT name, population FROM world
   WHERE name IN ('Sweden', 'Norway', 'Denmark');
--
+Lesson1 select
+SELECT name FROM world
+  WHERE name LIKE 'y%'
+  SELECT name FROM world
+  WHERE name LIKE '%y'
+  SELECT name FROM world
+  WHERE name LIKE '%x%'
+  SELECT name FROM world
+  WHERE name LIKE '%land'
+  SELECT name FROM world
+  WHERE name LIKE 'C%ia%'
+  SELECT name FROM world
+  WHERE name LIKE '%oo%'
+  SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+  SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+ 
   
+SELECT name FROM world
+ WHERE name LIKE '____'
+ SELECT name
+  FROM world
+ WHERE name LIKE capital
+ 
+ SELECT name
+  FROM world
+ WHERE concat(name, ' City') LIKE capital
+ SELECT capital,name
+  FROM world
+ WHERE capital LIKE concat('%',name,'%')
+ 
+ SELECT capital,name
+  FROM world
+ WHERE capital LIKE concat(name,'_','%')
+ SELECT name,REPLACE(capital,name,'') as ext
+  FROM world
+ WHERE capital LIKE concat(name,'_','%')
